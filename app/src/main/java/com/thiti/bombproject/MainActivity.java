3a8529f2a2da623f99f9319dab1af79f412ea5e3
Thibaut Delplanque <t.delplanque92@gmail.com>
2018-02-02 11:58:04 +0100

    add class Timer.java

--- app/src/main/java/com/thiti/bombproject/MainActivity.java
+++ app/src/main/java/com/thiti/bombproject/MainActivity.java
@@ -2,12 +2,20 @@ package com.thiti.bombproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextTime;
+    private TextView mLabelSaisie;
+    private Button mBtnControl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
---
+++ app/src/main/java/com/thiti/bombproject/Timer.java
@@ -0,0 +1,24 @@
+package com.thiti.bombproject;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+/**
+ * Created by canard on 02/02/18.
+ */
+
+public class Timer extends AsyncTask {
+
+    public Timer(Context context,int time,)
+
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        try {
+
+        }catch (){
+
+        }
+    }
+
+
+}
