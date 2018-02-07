e8c45f27eecda1504b9901783197dedcbfe0dba8
Thibaut Delplanque <t.delplanque92@gmail.com>
2018-02-07 21:49:10 +0100

    commentaires de l'interface TimeDisplay

--- app/src/main/java/com/thiti/bombproject/TimeDisplay.java
+++ app/src/main/java/com/thiti/bombproject/TimeDisplay.java
@@ -2,6 +2,10 @@ package com.thiti.bombproject;
 
 /**
  * Created by canard on 06/02/18.
+ * l'interface TimeDisplay est a implémenter si vous voulez faire des afficheurs pour
+ * le temps.
+ * Ceux qui permet au classe qui utilise des afficheur qui implémente TimeDisplay de ne pas se
+ * soucier de l'affichage avec la méthode display. ^^
  */
 
 public interface TimeDisplay {
